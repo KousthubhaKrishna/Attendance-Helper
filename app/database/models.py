f8168b4982115b8393f399fe7027a8523c3ec872
Kousthubha <krishnakousthubha@gmail.com>
2020-08-30 20:22:46 +0530

    Added Electives Options

--- app/database/models.py
+++ app/database/models.py
@@ -1,7 +1,18 @@
 from .databaseConfig import db
 
 class Student(db.Document):
-    roll = db.IntField(required=True,unique=True)
+    roll = db.StringField(required=True,unique=True)
     name = db.StringField(required=True)
-    section = db.StringField(required=True)
+    branch = db.StringField(required=True)
+    section = db.IntField(required=True)
     year = db.IntField(required=True)
+
+class Elective_students(db.Document):
+    eid = db.StringField(required=True, unique=True)
+    students_list = db.ListField()
+
+class Elective(db.Document):
+    branch = db.StringField(required=True)
+    year = db.IntField(required=True) 
+    eid = db.StringField(required=True, unique=True)
+    ename = db.StringField(required=True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app                                     
 from flask import render_template,request,request,redirect,send_file
-from .database.models import Student
+from .database.models import Student, Elective, Elective_students
 import codecs
 import pandas as pd
 import os
@@ -9,12 +9,21 @@ import traceback
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+
+    list_of_subjects = Elective.objects().all()
+
     if request.method == 'POST':
         try:
 
-            # Obtain the year and section selected and query for list of students
-            section, year = request.form['section'], request.form['year']
-            students_info = Student.objects(section=section,year=year).order_by('roll')
+            branch, section_list, year = request.form['branch'], request.form.getlist('section') , request.form['year']
+            section_list = [int(x) for x in section_list]
+            subject = request.form['subject']
+            print(section_list)
+
+            students_info = Student.objects(branch=branch,year=year,section__in = section_list).order_by('roll')
+            elective_students_list = []
+            if(subject != "-1"):
+                elective_students_list = Elective_students.objects.get(eid=int(subject)).students_list
 
             #Load the uploaded file, save, read and delete after work 
             uploaded_file = request.files['file']
@@ -26,25 +35,29 @@ def home():
                 att_data = pd.read_csv(codecs.open(file_url, 'rU', 'utf-16'),delimiter='\t')
                 os.remove(file_url)
             else:
-                return render_template("home.html", report=None, error="Please upload a file !")
+                return render_template("home.html", report=None, error="Please upload a file !",list_of_subjects=list_of_subjects)
 
             # Make a report by comparing data
-            report = prepare_report(students_info,att_data)
+            report = None
+            if(not elective_students_list):
+                report = prepare_report(students_info,att_data)
+            else:
+                report = prepare_report_for_electives(students_info,elective_students_list,att_data)
 
             # Save to downloads Folder
             file_download_url = app.config["DOWNLOADS_FOLDER"]+filename
             total_sheet = report["total_sheet"]
             total_sheet.to_csv(file_download_url, index = False, header=True)
 
-            return render_template("home.html", report=report,error="",file_download_url=filename,)
+            return render_template("home.html", report=report,error="",file_download_url=filename,list_of_subjects=list_of_subjects)
 
         # Any errors caused due to invalid data formats
         except Exception as error:
             print("Invalid Files",error)
             traceback.print_exc()
-            return render_template("home.html", report=None, error="Invalid file format !")
+            return render_template("home.html", report=None, error="Invalid file format !",list_of_subjects=list_of_subjects)
     else:
-        return render_template("home.html",report=None,error="")
+        return render_template("home.html",report=None,error="",list_of_subjects=list_of_subjects)
 
 
 def prepare_report(students_info,att_data):
@@ -57,6 +70,7 @@ def prepare_report(students_info,att_data):
     i = 1
     for index, student in enumerate(students_info):
         roll, name = student.roll, student.name[:20]
+
         if( name in present_set ):
             present_data.append({ "sno":pc, "roll":roll,"name":name })
             present_set.remove(name)
@@ -78,6 +92,44 @@ def prepare_report(students_info,att_data):
         "total_sheet":total_sheet,
     }
     return report
+
+
+def prepare_report_for_electives(students_info,elective_students_list,att_data):
+    elective_students_set = set([x[:15] for x in elective_students_list])
+    present_set = set([x[:20] for x in att_data["Full Name"]])
+    present_data,absent_data,unknown_data = [],[],[]
+
+    pc,ac = 1,1
+    total_sheet = []
+
+    i = 1
+    for index, student in enumerate(students_info):
+        roll, name = student.roll[:15], student.name[:20]
+
+        if(roll in elective_students_set):
+            if( name in present_set ):
+                present_data.append({ "sno":pc, "roll":roll,"name":name })
+                present_set.remove(name)
+                pc += 1
+                total_sheet.append([i,roll,name,1])
+            else:
+                absent_data.append({ "sno":ac, "roll":roll,"name":name})
+                ac += 1
+                total_sheet.append([i,roll,name,0])
+            i += 1
+        
+    total_sheet = pd.DataFrame(total_sheet,columns=['Sno','Roll','Name','Status'])
+    unknown_data = list(present_set)
+    unknown_data = [{"sno":i+1,"name":unknown_data[i]} for i in range(len(unknown_data))]
+    report = {
+        "present_data":present_data,
+        "absent_data":absent_data,
+        "unknown_data":unknown_data,
+        "total_sheet":total_sheet,
+    }
+    return report
+
+
 
 
 @app.route('/downloads/<filename>')
